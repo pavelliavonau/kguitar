67c14faed75e31402d710d80a6cdfb0146bfcea7
greycat <greycat@fe2163d1-5bc7-404b-90dc-a45a4846bd2b>
2010-02-05 11:04:38 +0000

    Ported Fretboard to Qt4 properly: now it works

--- kguitar/fretboard.h
+++ kguitar/fretboard.h
@@ -1,13 +1,9 @@
 #ifndef FRETBOARD_H
 #define FRETBOARD_H
 
-#include <qwidget.h>
-//Added by qt3to4:
-#include <QPaintEvent>
-#include <QResizeEvent>
-#include <QMouseEvent>
-#include <QPixmap>
 #include "global.h"
+
+#include <QWidget>
 
 class TabTrack;
 class QSizePolicy;
@@ -17,7 +13,7 @@ class QImage;
 class Fretboard: public QWidget {
 	Q_OBJECT
 public:
-	Fretboard(TabTrack *, QWidget *parent = 0, const char *name = 0);
+	Fretboard(TabTrack *, QWidget *parent = 0);
 	~Fretboard();
 
 public slots:
@@ -50,6 +46,16 @@ private:
 
 	int tonic;
 	int mode;
+
+	enum {
+		STRING_HEIGHT = 24,
+		ZERO_FRET_WIDTH = 24,
+		INLAY_RADIUS = 7,
+		FINGER_RADIUS = 8,
+		SIDE_BORDER = 2,
+		SCALE_BORDER = 5,
+		SCALE_ROUND = 70
+	};
 };
 
 #endif
